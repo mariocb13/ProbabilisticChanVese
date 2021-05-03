026af63642738ed4900f0bba23808f93fc0d2337
MrCorral13 <mariocorral1998@gmail.com>
2021-05-03 09:47:35 +0200

    Updates

--- functions/plot_normals.m
+++ functions/plot_normals.m
@@ -1,7 +1,7 @@
 function plot_normals(snake, N)
 %PLOT_NORMALS Function to plot the direction of the normals of the snake
 % points
-
+    
    delete(findobj('type','Line','-or','type','Quiver'))
    hold on
    quiver(snake(1:end,2), snake(1:end,1), N(:,2), N(:,1), 'b', 'LineWidth', 2);
